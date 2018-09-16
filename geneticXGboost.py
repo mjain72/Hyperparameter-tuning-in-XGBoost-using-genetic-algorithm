20edbbaff41359d0a06052d087cb46c87ea67ec7
Mohit Jain <mohitjain72@gmail.com>
2018-09-16 13:47:47 -0400

    Add files via upload

---
+++ geneticXGboost.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: mohit jain
+"""
+
+#create fitness function; will predict F1_score
+from sklearn.metrics import f1_score
+import numpy as np
+import random
+import xgboost as xgb
+import matplotlib.pyplot as plt
+
+
+random.seed(723)
+np.random.seed(723)
+
+def initilialize_poplulation(numberOfParents):
+    learningRate = np.empty([numberOfParents, 1])
+    nEstimators = np.empty([numberOfParents, 1], dtype = np.uint8)
+    maxDepth = np.empty([numberOfParents, 1], dtype = np.uint8)
+    minChildWeight = np.empty([numberOfParents, 1])
+    gammaValue = np.empty([numberOfParents, 1])
+    subSample = np.empty([numberOfParents, 1])
+    colSampleByTree =  np.empty([numberOfParents, 1])
+
+    for i in range(numberOfParents):
+        print(i)
+        learningRate[i] = round(random.uniform(0.01, 1), 2)
+        nEstimators[i] = random.randrange(10, 1500, step = 25)
+        maxDepth[i] = int(random.randrange(1, 10, step= 1))
+        minChildWeight[i] = round(random.uniform(0.01, 10.0), 2)
+        gammaValue[i] = round(random.uniform(0.01, 10.0), 2)
+        subSample[i] = round(random.uniform(0.01, 1.0), 2)
+        colSampleByTree[i] = round(random.uniform(0.01, 1.0), 2)
+    
+    population = np.concatenate((learningRate, nEstimators, maxDepth, minChildWeight, gammaValue, subSample, colSampleByTree), axis= 1)
+    return population
+    
+
+def fitness_f1score(y_true, y_pred):
+    fitness = round((f1_score(y_true, y_pred, average='weighted')), 4)
+    return fitness
+
+#train the data annd find fitness score
+def train_population(population, dMatrixTrain, dMatrixtest, y_test):
+    fScore = []
+    for i in range(population.shape[0]):
+        param = { 'objective':'binary:logistic',
+              'learning_rate': population[i][0],
+              'n_estimators': population[i][1], 
+              'max_depth': int(population[i][2]), 
+              'min_child_weight': population[i][3],
+              'gamma': population[i][4], 
+              'subsample': population[i][5],
+              'colsample_bytree': population[i][6],
+              'seed': 24}
+        num_round = 100
+        xgbT = xgb.train(param, dMatrixTrain, num_round)
+        preds = xgbT.predict(dMatrixtest)
+        preds = preds>0.5
+        fScore.append(fitness_f1score(y_test, preds))
+    return fScore
+
+
+
+#select parents for mating
+def new_parents_selection(population, fitness, numParents):
+    selectedParents = np.empty((numParents, population.shape[1])) #create an array to store fittest parents
+    
+    #find the top best performing parents
+    for parentId in range(numParents):
+        bestFitnessId = np.where(fitness == np.max(fitness))
+        bestFitnessId  = bestFitnessId[0][0]
+        selectedParents[parentId, :] = population[bestFitnessId, :]
+        fitness[bestFitnessId] = -1 #set this value to negative, in case of F1-score, so this parent is not selected again
+    return selectedParents
+        
+'''
+Mate these parents to create chilren having parameters from these parents (we are using uniorm crossover method)
+'''
+def crossover_uniform(parents, childrenSize):
+    
+    crossoverPointIndex = np.arange(0, np.uint8(childrenSize[1]), 1, dtype= np.uint8) #get all the index
+    crossoverPointIndex1 = np.random.randint(0, np.uint8(childrenSize[1]), np.uint8(childrenSize[1]/2)) # select half  of the indexes randomly
+    crossoverPointIndex2 = np.array(list(set(crossoverPointIndex) - set(crossoverPointIndex1))) #select leftover indexes
+    
+    children = np.empty(childrenSize)
+    
+    '''
+    Create child by choosing parameters from two paraents selected using new_parent_selection function. The parameter values
+    will be picked from the indexs, which were randomly selected above. 
+    '''
+    for i in range(childrenSize[0]):
+        
+        #find parent 1 index 
+        parent1_index = i%parents.shape[0]
+        #find parent 2 index
+        parent2_index = (i+1)%parents.shape[0]
+        #insert parameters based on random selected indexes in parent 1
+        children[i, crossoverPointIndex1] = parents[parent1_index, crossoverPointIndex1]
+        #insert parameters based on random selected indexes in parent 1
+        children[i, crossoverPointIndex2] = parents[parent2_index, crossoverPointIndex2]
+    return children
+    
+'''
+Introduce some mutation in the children. In case of XGboost we will introdcue mutation randomly on each parameter one at a time,
+based on which parameter is selected at random. Initially, we will define the maximum value that is allowed for the parameter, to prevent the
+out the range error during runtime. Subsequently, we will generate mutation value and add it to the parameter, and return the mutated offspring!!!
+'''
+
+def mutation(crossover, numberOfParameters):
+    #Define minimum and maximum values allowed for each parameter
+
+    minMaxValue = np.zeros((numberOfParameters, 2))
+    
+    minMaxValue[0:] = [0.01, 1.0] #min/max learning rate
+    minMaxValue[1, :] = [10, 2000] #min/max n_estimator
+    minMaxValue[2, :] = [1, 15] #min/max depth
+    minMaxValue[3, :] = [0, 10.0] #min/max child_weight
+    minMaxValue[4, :] = [0.01, 10.0] #min/max gamma
+    minMaxValue[5, :] = [0.01, 1.0] #min/maxsubsample
+    minMaxValue[6, :] = [0.01, 1.0] #min/maxcolsample_bytree
+ 
+    # Mutation changes a single gene in each offspring randomly.
+    mutationValue = 0
+    parameterSelect = np.random.randint(0, 7, 1)
+    print(parameterSelect)
+    if parameterSelect == 0: #learning_rate
+        mutationValue = round(np.random.uniform(-0.5, 0.5), 2)
+    if parameterSelect == 1: #n_estimators
+        mutationValue = np.random.randint(-200, 200, 1)
+    if parameterSelect == 2: #max_depth
+        mutationValue = np.random.randint(-5, 5, 1)
+    if parameterSelect == 3: #min_child_weight
+        mutationValue = round(np.random.uniform(5, 5), 2)
+    if parameterSelect == 4: #gamma
+        mutationValue = round(np.random.uniform(-2, 2), 2)
+    if parameterSelect == 5: #subsample
+        mutationValue = round(np.random.uniform(-0.5, 0.5), 2)
+    if parameterSelect == 6: #colsample
+        mutationValue = round(np.random.uniform(-0.5, 0.5), 2)
+  
+    #indtroduce mutation by changing one parameter, and set to max or min if it goes out of range
+    for idx in range(crossover.shape[0]):
+        crossover[idx, parameterSelect] = crossover[idx, parameterSelect] + mutationValue
+        if(crossover[idx, parameterSelect] > minMaxValue[parameterSelect, 1]):
+            crossover[idx, parameterSelect] = minMaxValue[parameterSelect, 1]
+        if(crossover[idx, parameterSelect] < minMaxValue[parameterSelect, 0]):
+            crossover[idx, parameterSelect] = minMaxValue[parameterSelect, 0]    
+    return crossover
+
+
+'''
+This function will allow us to genrate the heatmap for various parameters and fitness to visualize 
+how each paramter and fitness changes with each generation
+'''
+
+def plot_parameters(numberOfGenerations, numberOfParents, parameter, parameterName):
+    #inspired from https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
+    generationList = ["Gen {}".format(i) for i in range(numberOfGenerations+1)]
+    populationList = ["Parent {}".format(i) for i in range(numberOfParents)]
+    
+    fig, ax = plt.subplots()
+    im = ax.imshow(parameter, cmap=plt.get_cmap('YlOrBr'))
+    
+    # show ticks
+    ax.set_xticks(np.arange(len(populationList)))
+    ax.set_yticks(np.arange(len(generationList)))
+    
+    # show labels
+    ax.set_xticklabels(populationList)
+    ax.set_yticklabels(generationList)
+    
+    # set ticks at 45 degrees and rotate around anchor
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+    
+    
+    # insert the value of the parameter in each cell
+    for i in range(len(generationList)):
+        for j in range(len(populationList)):
+            text = ax.text(j, i, parameter[i, j],
+                           ha="center", va="center", color="k")
+    
+    ax.set_title("Change in the value of " + parameterName)
+    fig.tight_layout()
+    plt.show()
---
+++ xgboost_genetic.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: mohit jain
+"""
+
+'''
+We will use genetic algorithum to optimize hyperparameters for XGboost. 
+'''
+# Decision Tree Classification
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import geneticXGboost
+import xgboost as xgb
+
+
+np.random.seed(723)
+'''
+The dataset is from https://archive.ics.uci.edu/ml/machine-learning-databases/musk/
+It contains a set of 102 molecules, out of which 39 are identified by humans as 
+having odor that can be used in perfumery and 69 having not the desired odor.
+The dataset contains 6,590 low-energy conformations of these molecules, contianing 166 features.
+'''
+
+# Importing the dataset
+dataset = pd.read_csv('clean2.data', header=None)
+
+X = dataset.iloc[:, 2:168].values #discard first two coloums as these are molecule name and conformation name
+
+y = dataset.iloc[:, 168].values #extrtact last coloum as class (1 => desired odor, 0 => undesired odor)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 97)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+#XGboost Classifier
+
+#model xgboost
+#use xgboost API now
+xgDMatrix = xgb.DMatrix(X_train, y_train) #create Dmatrix
+xgbDMatrixTest = xgb.DMatrix(X_test, y_test)
+
+
+'''
+Let's find optimized parameters using genetic algorithms
+'''
+
+numberOfParents = 8 #number of parents to start
+numberOfParentsMating = 4 #number of parents that will mate
+numberOfParameters = 7 #number of parameters that will be optimized
+numberOfGenerations = 4 #number of genration that will be created
+
+#define the population size
+
+populationSize = (numberOfParents, numberOfParameters)
+
+#initialize the population with randomly generated parameters
+population = geneticXGboost.initilialize_poplulation(numberOfParents)
+
+#define an array to store the fitness  hitory
+fitnessHistory = np.empty([numberOfGenerations+1, numberOfParents])
+
+#define an array to store the value of each parameter for each parent and generation
+populationHistory = np.empty([(numberOfGenerations+1)*numberOfParents, numberOfParameters])
+
+#insert the value of initial parameters
+populationHistory[0:numberOfParents, :] = population
+
+for generation in range(numberOfGenerations):
+    print("This is number %s generation" % (generation))
+    
+    #train the dataset and obtain fitness
+    fitnessValue = geneticXGboost.train_population(population=population, dMatrixTrain=xgDMatrix, dMatrixtest=xgbDMatrixTest, y_test=y_test)
+    fitnessHistory[generation, :] = fitnessValue
+    
+    #best score in the current iteration
+    print('Best F1 score in the this iteration = {}'.format(np.max(fitnessHistory[generation, :])))
+
+    #survival of the fittest - take the top parents, based on the fitness value and number of parents needed to be selected
+    parents = geneticXGboost.new_parents_selection(population=population, fitness=fitnessValue, numParents=numberOfParentsMating)
+    
+    #mate these parents to create chilren having parameters from these parents (we are using uniorm crossover)
+    children = geneticXGboost.crossover_uniform(parents=parents, childrenSize=(populationSize[0] - parents.shape[0], numberOfParameters))
+    
+    #add mutation to create genetic diversity
+    children_mutated = geneticXGboost.mutation(children, numberOfParameters)
+    
+    '''
+    We will create new population, which will contain parents that where selected previously based on the
+    fitness score and rest of them  will be children
+    '''
+    population[0:parents.shape[0], :] = parents #fittest parents
+    population[parents.shape[0]:, :] = children_mutated #children
+    
+    populationHistory[(generation+1)*numberOfParents : (generation+1)*numberOfParents+ numberOfParents , :] = population #srore parent information
+    
+
+#Best solution from the final iteration
+
+fitness = geneticXGboost.train_population(population=population, dMatrixTrain=xgDMatrix, dMatrixtest=xgbDMatrixTest, y_test=y_test)
+fitnessHistory[generation+1, :] = fitness
+
+#index of the best solution
+bestFitnessIndex = np.where(fitness == np.max(fitness))[0][0]
+
+#Best fitness
+print("Best fitness is =", fitness[bestFitnessIndex])
+
+#Best parameters
+print("Best parameters are:")
+print('learning_rate', population[bestFitnessIndex][0])
+print('n_estimators', population[bestFitnessIndex][1])
+print('max_depth', int(population[bestFitnessIndex][2])) 
+print('min_child_weight', population[bestFitnessIndex][3])
+print('gamma', population[bestFitnessIndex][4])
+print('subsample', population[bestFitnessIndex][5])
+print('colsample_bytree', population[bestFitnessIndex][6])
+
+
+#visualize the change in fitness of the various generations and parents
+
+
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, fitnessHistory, "fitness (F1-score)")
+
+#Look at individual parameters change with generation
+#Create array for each parameter (Genration x Parents)
+
+learnigRateHistory = populationHistory[:, 0].reshape([numberOfGenerations+1, numberOfParents])
+nEstimatorHistory = populationHistory[:, 1].reshape([numberOfGenerations+1, numberOfParents])
+maxdepthHistory = populationHistory[:, 2].reshape([numberOfGenerations+1, numberOfParents])
+minChildWeightHistory = populationHistory[:, 3].reshape([numberOfGenerations+1, numberOfParents])
+gammaHistory = populationHistory[:, 4].reshape([numberOfGenerations+1, numberOfParents])
+subsampleHistory = populationHistory[:, 5].reshape([numberOfGenerations+1, numberOfParents])
+colsampleByTreeHistory = populationHistory[:, 6].reshape([numberOfGenerations+1, numberOfParents])
+
+#generate heatmap for each parameter
+
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, learnigRateHistory, "learning rate")
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, nEstimatorHistory, "n_estimator")
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, maxdepthHistory, "maximum depth")
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, minChildWeightHistory, "minimum child weight")
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, gammaHistory, "gamma")
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, subsampleHistory, "subsample")
+geneticXGboost.plot_parameters(numberOfGenerations, numberOfParents, colsampleByTreeHistory, "col sample by history")
+
